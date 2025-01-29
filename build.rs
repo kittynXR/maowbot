b5b5e75f671e4cabf0f3f0a954fd42eadbd8b9c6
kittyn <kittynvrc@gmail.com>
2025-01-29 16:30:45 +0900

    skeleton protobuf configuration

--- build.rs
+++ build.rs
@@ -1,11 +1,11 @@
 fn main() {
     tonic_build::configure()
         // Where to place generated code inside your src/ tree:
-        .out_dir("src/plugins/proto_gen")
+        .out_dir("src/plugins/proto")
         // Path to your proto + an include path (both point at "src/plugins")
         .compile(
-            &["src/plugins/plugin_service.proto"],
-            &["src/plugins"],
+            &["proto/plugin.proto"],
+            &["proto"],
         )
         .unwrap();
 }
